ee9ed90dd1455224ab55a3abfc11a19b87b4acdb
MAMIDIPAKA KISHOR <122218142+kkiisshhoorr@users.noreply.github.com>
2025-07-24 10:24:51 +0530

    Add files via upload

---
+++ java 23 july/hello.java
@@ -0,0 +1,285 @@
+
+public class hello {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+
+        // for(int i=0;i<=3;i++){
+        //     for(int j=0;j<=4;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+
+        // }
+
+
+
+
+
+
+        // for(int i=0;i<7;i++){
+        //     for(int j=0;j<5;j++){
+        //         if(i==0){
+        //             System.out.print("*");
+        //             }
+        //         else if(i==6){
+        //             System.out.print("*");
+        //         }
+        //         else if(i%2==0 && j==0 || i%2==0 && j==4){
+        //             System.out.print("*");
+        //         }
+        //         else{
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+
+
+
+        // for(int i=0;i<5;i++){
+        //     for(int j=0;j<=i;j++)
+        //     {
+        //         System.out.print("*");
+        //     }
+        //     System.err.println(" ");
+
+        // }
+
+
+
+
+
+
+
+
+
+
+        // for(int i=5;i>0;i--){
+        //     for(int j=i;j>0;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.err.println(" ");
+        // }
+
+
+
+
+
+
+
+
+        // for(int i=0;i<5;i++){
+        //     for(int k=5;k>0;k--){
+        //         if(i<k){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+
+        //     for(int j=0;j<=5;j++){
+        //         if(j<=i){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(" ");
+
+        // }
+
+
+
+
+
+
+
+
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if(i>=j){
+        //         System.out.print(j);}
+                
+        //     }
+        //     System.out.println("");
+        // }
+        
+
+
+
+
+
+
+
+
+        // for(int i=5;i>=1;i--){
+        //     for(int j=1;j<=5;j++){
+        //         if(i>=j){
+        //         System.out.print(j);}
+                
+        //     }
+        //     System.out.println("");
+        // }
+   
+
+
+
+
+
+
+
+        // int c=1;
+        // for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5;j++){
+        //         if (i>=j){
+        //             System.out.print(c);
+        //             c++;
+        //         }
+        //         System.out.print(" ");
+        //     }
+        //     System.out.println(" ");
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // for(int i=0;i<5;i++){
+        //     for(int k=5;k>0;k--){
+        //         if(i<k){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+
+        //     for(int j=0;j<=5;j++){
+        //         if(j<=i){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(" ");
+
+        // }
+
+
+
+
+
+
+        // for(int i=1;i<=4;i++){
+        //     for(int k=0;k<4;k++){
+        //         if(k<i){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+                
+        //     }
+
+        //     for(int j=4;j>0;j--){
+        //         if(i<j){
+        //             System.out.print(" ");
+        //         }
+        //         else {
+        //             System.out.print("*");}
+        //     }
+        //     System.out.println(" ");
+
+        // }
+        // for(int i=1;i<=4;i++){
+        //     for(int j=4;j>=0;j--){
+        //         if(j<=i){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+
+        //     }
+        //     for(int k=1;k<4;k++){
+        //         if(k<i){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+
+        //     System.out.println(" ");
+           
+
+        // }
+     
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // for(int i=0;i<5;i++){
+        //     for(int k=5;k>0;k--){
+        //         if(i<k){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+        //     }
+
+        //     for(int j=0;j<=5;j++){
+        //         if(j<=i){
+        //             System.out.print("*");
+        //         } else {
+        //             System.out.print(" ");
+        //         }
+        //     }
+        //     System.out.println(" ");
+
+        // }
+        // for(int i=1;i<5;i++){
+        //     for(int k=0;k<5;k++){
+        //         if(k<=i){
+        //             System.out.print(" ");
+        //         } else {
+        //             System.out.print("*");
+        //         }
+                
+        //     }
+
+        //     for(int j=0;j<5;j++){
+        //         if(j>=i){
+        //             System.out.print("*");
+        //         }
+        //     }
+        //     System.out.println(" ");
+
+        // }
+
+        
+
+
+
+
+    }
+}
---
+++ java 23 july/two.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+class two{
+    public static void main(String[] args) {
+        Scanner Sca = new Scanner(System.in);
+
+
+
+
+         // int sum = 0;
+        // for (int i = 1; i <= 10; i++) {
+        //     if (i % 2 == 0) {
+        //         sum += i;
+        //         System.out.println(i + " is even);
+        //     } else {
+        //         System.out.println(i + " is odd");
+        //     }
+        // }
+        // System.out.println("Total sum of even numbers from 1 to 10: " + sum);
+
+
+
+
+
+
+
+        // System.out.println("enter a number");
+        // int c= Sca.nextInt();
+        // if (c%3== 0 && c%5 == 0) {
+        //     System.out.println("divisible by both 3 and 5  "+c);
+        // } else if (c % 3 == 0) {
+        //     System.out.println("divisible by 3  "+c);
+        // } else if (c % 5 == 0) {
+        //     System.out.println("divisible by 5   "+c);
+        // } else {
+        //     System.out.println("not divisible by both 3 and 5"+c);
+        // }
+
+
+
+
+        // double c=Sca.nextDouble();
+        // System.out.println("double value is: " + c);
+        // int d= (int) c;
+        // System.out.println("int value is: " + d);
+        // byte e= (byte) c;
+        // System.out.println("byte value is: " + e);
+
+
+
+
+
+
+
+        // System.out.println("enter a number");
+        // int c=Sca.nextInt();
+        // for (int i = 1; i <= c; i++) {
+        //      for (int j = 1; j <= i; j++) {
+        //         System.out.print("*");
+        //     }
+        // System.out.println(); 
+        // }
+
+            
+
+
+
+
+        // for (int i=1;i<=100;i++){
+        //     for(int j=2;j<=Math.sqrt(i);j++){
+        //         if(i%j==0){
+        //             System.out.println(i+" is not prime");
+        //             break;
+        //         }
+        //         else{
+        //             System.out.println(i+" is prime");
+        //             break;
+        //         }
+        //     }
+        // }
+
+    }
+}
